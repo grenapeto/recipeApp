27a056f8971a64ab267c0046d6efcc2b5d0b1c94
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-07-24 17:30:02 +0200

    navbar added

--- recipe-book/src/app/app.component.ts
+++ recipe-book/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatButtonModule, RouterModule ],
+  imports: [CommonModule, RouterOutlet, MatButtonModule, RouterModule, MatToolbarModule, MatIconModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
