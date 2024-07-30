2d8422b68746ec1a9092e81c8b2093fb2f59d8a1
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-07-30 10:23:05 +0200

    details in progress

--- recipe-book/src/app/recipe-detail/recipe-detail.component.ts
+++ recipe-book/src/app/recipe-detail/recipe-detail.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { RecipeService } from '../recipe.service';
 import { MOCK_RECIPES } from '../mock-recipes';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { CommonModule, NgFor } from '@angular/common';
 @Component({
   selector: 'app-recipe-detail',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDialogModule, NgFor, CommonModule],
   templateUrl: './recipe-detail.component.html',
-  styleUrl: './recipe-detail.component.css'
+  styleUrl: './recipe-detail.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RecipeDetailComponent {
   recipes = MOCK_RECIPES;
+
+  readonly dialog = inject(MatDialog);
+
+  showMore() {
+    const dialogRef = this.dialog.open(ShowMorePopup);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
+
+@Component({
+  selector: 'show-more-popup',
+  templateUrl: 'show-more-popup.component.html',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ShowMorePopup {
+  recipeDescription = MOCK_RECIPES;
+}
+
