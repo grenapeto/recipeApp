ce7ed53cd9404cfa29129a63131c7c9577047ed6
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-08-02 11:02:03 +0200

    recipe-form working but looking bad

--- recipe-book/src/app/recipe-form/recipe-form.component.ts
+++ recipe-book/src/app/recipe-form/recipe-form.component.ts
@@ -1,3 +1,54 @@
+// import { Component } from '@angular/core';
+// import { RecipeService } from '../recipe.service';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatDialogModule } from '@angular/material/dialog';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatCardModule } from '@angular/material/card';
+// import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+// import { CommonModule } from '@angular/common';
+// import { Observable } from 'rxjs';
+
+// @Component({
+//   selector: 'app-recipe-form',
+//   standalone: true,
+//   imports: [
+//     MatButtonModule,
+//     MatDialogModule,
+//     MatInputModule,
+//     MatFormFieldModule,
+//     MatCardModule,
+//     ReactiveFormsModule,
+//     CommonModule,
+//   ],
+//   templateUrl: './recipe-form.component.html',
+//   styleUrls: ['./recipe-form.component.css']
+// })
+// export class RecipeFormComponent {
+//   recipeForm: FormGroup;
+// lastAddedRecipe: any = null;
+
+//   constructor(private fb: FormBuilder, private recipeService: RecipeService) {
+//     this.recipeForm = this.fb.group({
+//       name: [''],
+//       ingredients: [''],
+//       instructions: ['']
+//     });
+
+
+
+//   }
+
+//   onSubmit() {
+//     if (this.recipeForm.valid) {
+//       this.recipeService.addRecipe(this.recipeForm.value).subscribe(result => {
+//         console.log('Recipe added:', result);
+//         this.recipeForm.reset();
+//         this.lastAddedRecipe = result; 
+//       });
+//     }
+//   }
+// }
 import { Component } from '@angular/core';
 import { RecipeService } from '../recipe.service';
 import { MatButtonModule } from '@angular/material/button';
@@ -5,8 +56,9 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-recipe-form',
@@ -18,20 +70,45 @@ import { CommonModule } from '@angular/common';
     MatFormFieldModule,
     MatCardModule,
     ReactiveFormsModule,
-    CommonModule,
+    CommonModule, MatIcon,
   ],
   templateUrl: './recipe-form.component.html',
   styleUrls: ['./recipe-form.component.css']
 })
 export class RecipeFormComponent {
   recipeForm: FormGroup;
+  lastAddedRecipe: any = null;
 
   constructor(private fb: FormBuilder, private recipeService: RecipeService) {
     this.recipeForm = this.fb.group({
       name: [''],
-      ingredients: [''],
-      instructions: ['']
+      ingredients: this.fb.array([]),  // Use FormArray for ingredients
+      instructions: this.fb.array([])  // Use FormArray for instructions
     });
+  }
+
+  get ingredients(): FormArray {
+    return this.recipeForm.get('ingredients') as FormArray;
+  }
+
+  get instructions(): FormArray {
+    return this.recipeForm.get('instructions') as FormArray;
+  }
+
+  addIngredient() {
+    this.ingredients.push(this.fb.control(''));
+  }
+
+  removeIngredient(index: number) {
+    this.ingredients.removeAt(index);
+  }
+
+  addInstruction() {
+    this.instructions.push(this.fb.control(''));
+  }
+
+  removeInstruction(index: number) {
+    this.instructions.removeAt(index);
   }
 
   onSubmit() {
@@ -39,9 +116,19 @@ export class RecipeFormComponent {
       this.recipeService.addRecipe(this.recipeForm.value).subscribe(result => {
         console.log('Recipe added:', result);
         this.recipeForm.reset();
+        this.lastAddedRecipe = result; 
+        this.resetFormArrays();
       });
     }
   }
+
+  private resetFormArrays() {
+    // Reset the form arrays to be empty after submission
+    while (this.ingredients.length) {
+      this.ingredients.removeAt(0);
+    }
+    while (this.instructions.length) {
+      this.instructions.removeAt(0);
+    }
+  }
 }
-
-
--- recipe-book/src/app/recipe.service.ts
+++ recipe-book/src/app/recipe.service.ts
@@ -1,30 +1,76 @@
+// import { Injectable } from '@angular/core';
+// import { Observable, of } from 'rxjs';
+// import { MOCK_RECIPES } from './mock-recipes';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class RecipeService {
+//   private recipes = MOCK_RECIPES;
+
+//   constructor() { }
+
+//   getRecipes(): Observable<any[]> {
+//     return of(this.recipes);
+//   }
+
+//   getRecipeById(id: number): Observable<any> {
+//     const recipe = this.recipes.find((r) => r.id === id);
+//     return of(recipe);
+//   }
+
+//   addRecipe(recipe: any): Observable<any> {
+//     recipe.id = this.recipes.length + 1;
+//     this.recipes.push(recipe);
+//     return of(recipe);
+//   }
+
+//   updateRecipe(updatedRecipe: any): Observable<any> {
+//     const index = this.recipes.findIndex((r) => r.id === updatedRecipe.id);
+//     if (index > -1) {
+//       this.recipes[index] = updatedRecipe;
+//       return of(updatedRecipe);
+//     }
+//     return of(null);
+//   }
+
+//   deleteRecipe(id: number): Observable<any> {
+//     this.recipes = this.recipes.filter((r) => r.id !== id);
+//     return of({ id });
+//   }
+// }
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { MOCK_RECIPES } from './mock-recipes';
+import { MOCK_RECIPES } from './mock-recipes'; // Ensure this is an array of recipe objects
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
-  private recipes = MOCK_RECIPES;
+  private recipes = MOCK_RECIPES || []; // Ensure an empty array if undefined
 
-  constructor() { }
+  constructor() {}
 
+  // Get all recipes
   getRecipes(): Observable<any[]> {
     return of(this.recipes);
   }
 
+  // Get a recipe by ID
   getRecipeById(id: number): Observable<any> {
     const recipe = this.recipes.find((r) => r.id === id);
     return of(recipe);
   }
 
+  // Add a new recipe
   addRecipe(recipe: any): Observable<any> {
-    recipe.id = this.recipes.length + 1;
+    recipe.id = this.recipes.length ? Math.max(...this.recipes.map(r => r.id)) + 1 : 1; // Ensure unique ID
     this.recipes.push(recipe);
+    console.log('Recipes after addition:', this.recipes); // Debugging: Check recipes array
     return of(recipe);
   }
 
+  // Update an existing recipe
   updateRecipe(updatedRecipe: any): Observable<any> {
     const index = this.recipes.findIndex((r) => r.id === updatedRecipe.id);
     if (index > -1) {
@@ -34,6 +80,7 @@ export class RecipeService {
     return of(null);
   }
 
+  // Delete a recipe by ID
   deleteRecipe(id: number): Observable<any> {
     this.recipes = this.recipes.filter((r) => r.id !== id);
     return of({ id });
