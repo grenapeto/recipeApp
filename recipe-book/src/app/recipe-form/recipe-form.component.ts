7f7e9612c01c3fe6afb51d651ebe97be416002ae
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-08-02 17:29:19 +0200

    image not working

--- recipe-book/src/app/recipe-form/recipe-form.component.ts
+++ recipe-book/src/app/recipe-form/recipe-form.component.ts
@@ -8,6 +8,8 @@ import { MatCardModule } from '@angular/material/card';
 import { FormBuilder, FormGroup, FormArray, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
+import { UploaderModule } from 'angular-uploader';
+import { Uploader, UploadWidgetConfig, UploadWidgetResult } from 'uploader';
 
 @Component({
   selector: 'app-recipe-form',
@@ -19,20 +21,44 @@ import { MatIcon } from '@angular/material/icon';
     MatFormFieldModule,
     MatCardModule,
     ReactiveFormsModule,
-    CommonModule, MatIcon
+    CommonModule,
+    MatIcon,
+    UploaderModule
   ],
   templateUrl: './recipe-form.component.html',
   styleUrls: ['./recipe-form.component.css']
 })
 export class RecipeFormComponent {
+  uploader = Uploader({
+    apiKey: 'free', // <-- Get production-ready API keys from Bytescale
+  });
+  options: UploadWidgetConfig = {
+    multi: false,
+  };
+  
+  onComplete = (files: UploadWidgetResult[]) => {
+    if (files.length > 0) {
+      const uploadedUrl = files[0].fileUrl;
+      console.log('Uploaded file URL:', uploadedUrl);
+      this.images.push(this.fb.control(uploadedUrl));  // Add URL to the form array
+    }
+  };
+
+  // Declare uploadedFileUrl to accept both string and undefined
+  uploadedFileUrl: string | undefined = undefined;
+
   recipeForm: FormGroup;
   lastAddedRecipe: any = null;
+  height!: string;
+  width!: string;
+  onUpdate: ((files: UploadWidgetResult[]) => void) | undefined;
 
   constructor(private fb: FormBuilder, private recipeService: RecipeService) {
     this.recipeForm = this.fb.group({
       name: [''],
       ingredients: this.fb.array([]),  // Use FormArray for ingredients
-      instructions: this.fb.array([])  // Use FormArray for instructions
+      instructions: this.fb.array([]),  // Use FormArray for instructions
+      images: this.fb.array([])  // Add FormArray for images
     });
   }
 
@@ -42,6 +68,10 @@ export class RecipeFormComponent {
 
   get instructions(): FormArray {
     return this.recipeForm.get('instructions') as FormArray;
+  }
+
+  get images(): FormArray {
+    return this.recipeForm.get('images') as FormArray;
   }
 
   addIngredient() {
@@ -58,6 +88,10 @@ export class RecipeFormComponent {
 
   removeInstruction(index: number) {
     this.instructions.removeAt(index);
+  }
+
+  removeImage(index: number) {
+    this.images.removeAt(index);
   }
 
   onSubmit() {
@@ -79,12 +113,9 @@ export class RecipeFormComponent {
     while (this.instructions.length) {
       this.instructions.removeAt(0);
     }
+    while (this.images.length) {
+      this.images.removeAt(0);
+    }
   }
 }
-
-
-
-
-
-
 
