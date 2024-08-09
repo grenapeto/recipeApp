818bedc66ea7c62ad8124d1cbde94162d7ac7419
PatrycjaJakubow <106872766+PatrycjaJakubow@users.noreply.github.com>
2024-08-09 11:00:55 +0200

    api working in recipe list component

--- recipe-book/src/app/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RecipeService } from '../recipe.service';
 import { MatListModule } from '@angular/material/list';
 import { CommonModule } from '@angular/common';
@@ -6,6 +6,7 @@ import { MatCardModule } from '@angular/material/card';
 import { RecipeDetailComponent } from '../recipe-detail/recipe-detail.component';
 import { DetailsComponent } from '../details/details.component';
 import { MatIconModule } from '@angular/material/icon';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-recipe-list',
@@ -16,7 +17,9 @@ import { MatIconModule } from '@angular/material/icon';
     MatCardModule, 
     RecipeDetailComponent,
     DetailsComponent,
-    MatIconModule
+    MatIconModule,
+
+    
   ],
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'] 
@@ -24,12 +27,25 @@ import { MatIconModule } from '@angular/material/icon';
 export class RecipeListComponent implements OnInit {
 
   recipes: any[] = [];
-
+httpClient = inject(HttpClient); //nowe
+data: any[] = []
   constructor(private recipeService: RecipeService) {}
 
-  ngOnInit() {
+  ngOnInit():void {
+    this.fetchData(); //nowe
     this.loadRecipes();
+   
   }
+
+  fetchData() {
+    this.httpClient.get('https://api.edamam.com/api/recipes/v2?type=public&app_id=d78a8854&app_key=%20e8e08c0ff7ca76b4c80dccce32b4f755%09&cuisineType=American&cuisineType=Asian&cuisineType=British&cuisineType=Central%20Europe&cuisineType=Chinese&cuisineType=Eastern%20Europe&cuisineType=French&cuisineType=Indian&cuisineType=Italian&cuisineType=Japanese&cuisineType=Kosher&cuisineType=Mediterranean&cuisineType=Mexican&cuisineType=Middle%20Eastern&cuisineType=Nordic&cuisineType=South%20American&cuisineType=South%20East%20Asian&mealType=Breakfast&mealType=Dinner&mealType=Lunch&mealType=Snack&dishType=Biscuits%20and%20cookies&dishType=Bread&dishType=Cereals&dishType=Condiments%20and%20sauces&dishType=Desserts&dishType=Drinks&dishType=Main%20course&dishType=Pancake&dishType=Sandwiches&dishType=Side%20dish&dishType=Soup&dishType=Starter&dishType=Sweets&imageSize=REGULAR&imageSize=SMALL')
+    .subscribe((data: any) => {
+console.log(data);
+this.data = data.hits.map((hit: any) => hit.recipe);
+
+    }); ///nowe
+  }
+
 
   loadRecipes() {
     this.recipeService.getRecipes().subscribe(data => {
